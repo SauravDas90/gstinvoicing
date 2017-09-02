4a3df9f2b0544148252f439aa72fff9218db1ff7
Kaustabh <kaustabh93@gmail.com>
2017-09-02 18:46:17 +0530

    Majority of UI

--- gstproject/routes/dashboard.js
+++ gstproject/routes/dashboard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Goods & Services Tax' });
+  res.render('dashboard', { title: 'GST Dashboard' });
 });
 
 module.exports = router;
--- gstproject/routes/login.js
+++ gstproject/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Goods & Services Tax' });
+  res.render('login', { title: 'Goods & Service Tax' });
 });
 
 module.exports = router;
